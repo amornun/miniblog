e4024223ed1ef66b3d78fa83226325871f9b1e97
amornun <amornun.profe@gmail.com>
2023-02-08 13:54:42 +0100

    enrutado

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
+var bodyParser = require("body-parser");
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts');
+
+var mongoose = require('mongoose');
+mongoose.set('strictQuery', false); //requerido para quitar el warning
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
+    .then(() => console.log('connection successful'))
+    .catch((err) => console.error(err));
+mongoose.connection;
 
 var app = express();
 
@@ -15,13 +25,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.json()); //Ya no es necesario
+//app.use(express.urlencoded({ extended: false })); //Ya no es necesario
+app.use(cors());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,7 +51,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  //res.render('error');
+  res.json({ message: err.message, error: err });
 });
 
 module.exports = app;
